613734b11243d3e1a3fbe8ceadecbbf2915697d1
Rainrev893 <rainrevcarbonell30@gmail.com.com>
2026-02-23 22:02:25 +0800

    Backend ready for deployment

--- api-server/db.js
+++ api-server/db.js
@@ -2,8 +2,16 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const db = await mysql.createPool({
+console.log("DB CONFIG:", {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME
+});
+
+export const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
