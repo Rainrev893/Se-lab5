42ab15d90470b072265d2927e23bcf361aac91d9
Rainrev893 <rainrevcarbonell30@gmail.com>
2026-03-07 11:18:33 +0800

    fix 2.0

--- mental-health-wellness/src/services/api.js
+++ mental-health-wellness/src/services/api.js
@@ -1,9 +1,10 @@
+// mental-health-wellness/src/services/api.js
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://lab-6-oajw.onrender.com/api',
+  baseURL: 'https://lab-6-oajw.onrender.com', // Remove the /api here
   headers: {
     "Content-Type": "application/json",
   },
-  timeout: 15000,  
+  timeout: 60000,  
 });
